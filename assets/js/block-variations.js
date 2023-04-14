9b0b93fe283a8b800a7abb8b4615a9790eb87d19
Jonathan Roley <jonathan.roley@gmail.com>
2023-04-14 14:14:24 -0400

    design system changes

--- assets/js/block-variations.js
+++ assets/js/block-variations.js
@@ -1,32 +1,47 @@
 
-wp.blocks.registerBlockStyle( 
-  'core/button', 
-  {
-    name: "small-button",
-    label: "Small"
-  }
-)
+wp.domReady(() => {
 
-wp.blocks.registerBlockStyle( 
-  'core/button', 
-  {
-    name: "small-outline-button",
-    label: "Small Outline"
-  }
-)
+  wp.blocks.registerBlockStyle( 
+    'core/button', 
+    {
+      name: "outline-button",
+      label: "Outline"
+    }
+  )
 
-wp.blocks.registerBlockStyle( 
-  'core/button', 
-  {
-    name: "large-button",
-    label: "Large"
-  }
-)
+  wp.blocks.registerBlockStyle( 
+    'core/button', 
+    {
+      name: "small-button",
+      label: "Small"
+    }
+  )
 
-wp.blocks.registerBlockStyle( 
-  'core/button', 
-  {
-    name: "large-outline-button",
-    label: "Large Outline"
-  }
-)
+  wp.blocks.registerBlockStyle( 
+    'core/button', 
+    {
+      name: "small-outline-button",
+      label: "Small Outline"
+    }
+  )
+
+  wp.blocks.registerBlockStyle( 
+    'core/button', 
+    {
+      name: "large-button",
+      label: "Large"
+    }
+  )
+
+  wp.blocks.registerBlockStyle( 
+    'core/button', 
+    {
+      name: "large-outline-button",
+      label: "Large Outline"
+    }
+  )
+
+  // default outline button has styles applied
+  wp.blocks.unregisterBlockStyle( 'core/button', 'outline' )
+})
+
